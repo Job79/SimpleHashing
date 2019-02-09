7bb4107a9c119d8a75bb3f840e51784d6506cdd6
Henkje <henkje@pm.me>
2019-02-09 14:20:35 +0100

    Removed Length from verify and changed comments

--- PBKDF2.cs
+++ PBKDF2.cs
@@ -1,5 +1,5 @@
 /* PBKDF2
- * Copyright (C) 2018  henkje (henkje@pm.me)
+ * Copyright (C) 2019  henkje (henkje@pm.me)
  * 
  * MIT license
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
@@ -15,41 +15,68 @@ using System.Security.Cryptography;
 
 namespace Hashing
 {
-    class PBKDF2
+    public static class PBKDF2
     {
-        public static string Hash(string Password, int Iterations = 10000, int Length = 20)
+        /// <summary>
+        /// Hash input with PBKDF2.
+        /// </summary>
+        /// <param name="Input">String to hash</param>
+        /// <param name="Iterations">Rounds PBKDF2 will make to genarete the hash</param>
+        /// <param name="Length">Lenth of the hash, the output will also contain the salt</param>
+        /// <returns></returns>
+        public static string Hash(string Input, int Iterations = 10000, int Length = 20)
         {
-            //create salt of 16 byte's
+            if (string.IsNullOrEmpty(Input)) throw new Exception("Could not hash input: Input is empty.");
+            else if (Iterations <= 0) throw new Exception("Could not hash input: Iterations can't be negative or null.");
+            else if (Length <= 0) throw new Exception("Could not hash input: Length can't be negative or null.");
+
+            //Create salt of 16 byte's.
             byte[] Salt = new byte[16];
             new RNGCryptoServiceProvider().GetBytes(Salt);
 
-            //create hash of 20 byte's
-            byte[] Hash = new Rfc2898DeriveBytes(Password, Salt, Iterations).GetBytes(Length);
+            //Create hash of 20 byte's.
+            byte[] Hash = new Rfc2898DeriveBytes(Input, Salt, Iterations).GetBytes(Length);
 
-            //combine salt and hash
+            //Combine salt and hash.
             byte[] CombinedBytes = new byte[16 + Length];
             Buffer.BlockCopy(Salt, 0, CombinedBytes, 0, 16);
             Buffer.BlockCopy(Hash, 0, CombinedBytes, 16, Length);
 
-            //return string
+            //Return CombinedBytes as base64 string.
             return Convert.ToBase64String(CombinedBytes);
         }
 
-        public static bool Verify(string Password, string HashedPassword, int Iterations = 10000, int Length = 20)
+        /// <summary>
+        /// Verify hashed string with another string.
+        /// </summary>
+        /// <param name="HashedInput">Hashed string</param>
+        /// <param name="Input">String to compare with HashedInput</param>
+        /// <param name="Iterations">Rounds PBKDF2 made to generate HashedInput</param>
+        /// <returns></returns>
+        public static bool Verify(string HashedInput, string Input, int Iterations = 10000)
         {
-            //convert to byte's
-            byte[] HashedBytes = Convert.FromBase64String(HashedPassword);
+            if (string.IsNullOrEmpty(Input)) throw new Exception("Could not hash input: HashedInput is empty.");
+            else if (string.IsNullOrEmpty(Input)) throw new Exception("Could not hash input: Input is empty.");
+            else if (Iterations <= 0) throw new Exception("Could not hash input: Iterations can't be negative or null.");
 
-            //get the salt from byte's
+            //Get byte's from HashedInput.
+            byte[] HashedBytes = Convert.FromBase64String(HashedInput);
+
+            //Get the length of the hash.
+            int Length = HashedBytes.Length - 16;
+            Console.WriteLine(Length);
+
+            //Get the salt from HashedInputBytes.
             byte[] Salt = new byte[16];
             Buffer.BlockCopy(HashedBytes, 0, Salt, 0, 16);
 
-            //generate hash of password, to compare it with hashedPassword
-            byte[] Hash = new Rfc2898DeriveBytes(Password, Salt, 10000).GetBytes(Length);
+            //Generate hash of Input, to compare it with HashedInput
+            byte[] Hash = new Rfc2898DeriveBytes(Input, Salt, Iterations).GetBytes(Length);
 
-            //compare the result's
+            //Compare the result's.
             for (int i = 0; i < Length; i++)
                 if (HashedBytes[i + 16] != Hash[i]) return false;
+
             return true;
         }
     }
